e076b2226f5c418a14093f183cdd46466a03c380
thaidv96 <thaidv96@gmail.com>
2023-06-04 17:05:02 +0700

    disable requirements

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ setup(name='lasotuvi',
       setup_requires=['pytest-runner'],
       tests_require=['pytest'],
       install_requires=[
-          "attrs==17.4.0",
-          "ephem==3.7.6.0  ; sys_platform == 'linux'",
-          "more-itertools==4.1.0",
-          "mypy==0.580",
-          "pluggy==0.6.0",
-          "py==1.10.0",
-          "pytest==3.5.0",
-          "six==1.11.0",
-          "typed-ast==1.1.0"
+        #   "attrs==17.4.0",
+        #   "ephem==3.7.6.0  ; sys_platform == 'linux'",
+        #   "more-itertools==4.1.0",
+        #   "mypy==0.580",
+        #   "pluggy==0.6.0",
+        #   "py==1.10.0",
+        #   "pytest==3.5.0",
+        #   "six==1.11.0",
+        #   "typed-ast==1.1.0"
       ],
       zip_safe=False)
